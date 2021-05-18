5a0e81360c87c99251f5265623e30fd2ed8e0003
lazsloo <trtaylor2024@gmail.com>
2021-05-17 23:46:25 -0700

    first commit

--- JavaScript/Loops_Algo/Loops_Algo.js
+++ JavaScript/Loops_Algo/Loops_Algo.js
@@ -9,7 +9,7 @@ for (var odd = 0; odd < 20; odd++) {
 // 2. Sum and Print 1-5
 var sum = 1
 for (var num = 1; num <= 5; num++) {
-    console.log("num" + num + " " + "sum" + sum)
+    console.log("num:" + num + " " + "sum:" + sum)
     if (sum = sum + 1 + num) {
     }
 }
