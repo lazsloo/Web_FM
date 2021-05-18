27fa07efb9562507327f447ea74d5cb7131c8afb
lazsloo <trtaylor2024@gmail.com>
2021-05-17 23:39:03 -0700

    first commit

---
+++ JavaScript/Loops_Algo/Loops_Algo.js
@@ -0,0 +1,16 @@
+// 1. Print odds 1-20
+for (var odd = 0; odd < 20; odd++) {
+    if (odd % 2 === 0) {
+    } else {
+        console.log(odd)
+    }
+}
+
+// 2. Sum and Print 1-5
+var sum = 1
+for (var num = 1; num <= 5; num++) {
+    console.log("num" + num + " " + "sum" + sum)
+    if (sum = sum + 1 + num) {
+    }
+}
+
