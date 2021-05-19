5caffdcf5f7e158f9982ec7ac31af13d42686827
lazsloo <trtaylor2024@gmail.com>
2021-05-18 23:23:53 -0700

    first commit

--- JavaScript/Array_Algo/Array_Algo.js
+++ JavaScript/Array_Algo/Array_Algo.js
@@ -12,7 +12,7 @@
 var testArr = [6,3,5,1,2,4];
 var sum = 0;
 for(var i = 0; i < testArr.length; i++) {
-    sum+=testArr[i];
+    sum +=testArr[i];
     console.log('num:', testArr[i], 'sum:', sum)
 }
 
